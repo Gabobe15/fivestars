70a68a071d0c57abd628ac2cce104da793cb1233
Gabobe15 <gabobe33@gmail.com>
2023-08-31 14:22:10 +0300

    second added trucks and contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,81 @@
-import { Grid } from "@mui/material"
+import { Box, Container, Grid, Typography } from "@mui/material"
 // import Form from "./Form"
 // import Form2 from './Form2'
 import  Form  from "./Form"
+import { MdOutlineMailOutline, MdPhoneCallback } from "react-icons/md";
+import { BsFillHouseFill } from "react-icons/bs";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Contact = () => {
   return (
-    <Grid container spacing={2} maxWidth="lg">
+    <Container spacing={2} sx={{ minHeight: "100vh" }}>
+      <Grid container spacing={2} maxWidth="lg">
         <Grid item lg="6" md="12">
-
+          <Box sx={{mb:'20px'}}>
+            <Typography variant="h5" component="h6">
+              Main Branch
+            </Typography>
+            <Box>
+              <Typography variant="body">
+                <MdPhoneCallback className="icons" /> +1(302) 425-93-15
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body">
+                <MdOutlineMailOutline className="icons" /> info@fivestar.com
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body">
+                <BsFillHouseFill className="icons" />
+                FIVE STAR
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body">
+                <FaLocationDot className="icons" /> 3524 SILVERSIDE RD STE 35B
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body"> WILMINGTON, DE</Typography>
+            </Box>
+          </Box>
+          <Box>
+            <Typography variant="h5" component="h6">
+              Other Branch
+            </Typography>
+            <Box>
+              <Typography variant="body">
+                <MdPhoneCallback className="icons" /> +1(302) 425-93-15
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body">
+                <MdOutlineMailOutline className="icons" /> info@fivestar.com
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body">
+                <BsFillHouseFill className="icons" />
+                FIVE STAR
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body">
+                <FaLocationDot className="icons" /> 3524 SILVERSIDE RD STE 35B
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body"> WILMINGTON, DE</Typography>
+            </Box>
+          </Box>
         </Grid>
         <Grid item lg="6" md="12">
-          <Form/>
+          <Form />
         </Grid>
-    </Grid>
-  )
+      </Grid>
+    </Container>
+  );
 }
 
 export default Contact
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,70 @@
+import { Typography, Card,CardMedia, CardContent, Container, Grid } from "@mui/material"
+import disImg from '../assets/billing.jpeg'
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
+    <Container sx={{ minHeight: "100vh", pb:'30px' }}>
+      <Typography variant="h3">Services</Typography>
+      <Grid container spacing={4} sx={{margin:'0 auto'}} >
+        <Grid item md="5" sm="12">
+          <Card>
+            <CardMedia>
+              <img src={disImg} alt="billing" style={{ width: "100%" }} />
+            </CardMedia>
+            <CardContent>
+              <Typography variant="h5">Dispatcher</Typography>
+              <Typography variant="body2" color="text.secondary">
+                some dispatch services require a contract. That contract can be
+                short, as is the case of a week when billing a flat weekly
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md="5" sm="12">
+          <Card>
+            <CardMedia>
+              <img src={disImg} alt="billing" style={{ width: "100%" }} />
+            </CardMedia>
+            <CardContent>
+              <Typography variant="h5">Dispatcher</Typography>
+              <Typography variant="body2" color="text.secondary">
+                some dispatch services require a contract. That contract can be
+                short, as is the case of a week when billing a flat weekly
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md="5" sm="12">
+          <Card>
+            <CardMedia>
+              <img src={disImg} alt="billing" style={{ width: "100%" }} />
+            </CardMedia>
+            <CardContent>
+              <Typography variant="h5">Dispatcher</Typography>
+              <Typography variant="body2" color="text.secondary">
+                some dispatch services require a contract. That contract can be
+                short, as is the case of a week when billing a flat weekly
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md="5" sm="12">
+          <Card>
+            <CardMedia>
+              <img src={disImg} alt="billing" style={{ width: "100%" }} />
+            </CardMedia>
+            <CardContent>
+              <Typography variant="h5">Dispatcher</Typography>
+              <Typography variant="body2" color="text.secondary">
+                some dispatch services require a contract. That contract can be
+                short, as is the case of a week when billing a flat weekly
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Services
