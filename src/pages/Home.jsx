75337a003195fd3df40d4727e3e7f926b63722df
Gabobe15 <gabobe33@gmail.com>
2023-08-31 21:36:07 +0300

    change button color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,67 @@
 import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import redtruck from "../assets/redtruck.jpg";
 
+// import pages 
+import { About, Contact, Services  } from '../pages'
+
 const Home = () => {
   return (
-    <Box
-      sx={{
-        // backgroundColor: "blue",
-        minHeight: "100vh",
-        width: "100%",
-        marginTop: "-60px",
-        backgroundImage: `url(${redtruck})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        objectFit: "fit",
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid container spacing={2} sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-          <Grid item md="6" sm="12" sx={{marginTop:'60px', padding:'0 10px'}}>
-            <Typography
-              variant="h1"
-              sx={{ margin: "40px 0 auto", fontWeight: 400, color: "#fff" }}
-            >
-              FREIGHT DISPATCH SERVICE
-            </Typography>
-          </Grid>
+    <Box>
+      <Box
+        sx={{
+          // backgroundColor: "blue",
+          minHeight: "100vh",
+          width: "100%",
+          marginTop: "-60px",
+          backgroundImage: `url(${redtruck})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          objectFit: "fit",
+        }}
+      >
+        <Container maxWidth="lg">
           <Grid
-            item
-            md="6"
-            sm="12"
-            sx={{fontWeight: 400, color: "#fff", marginTop:'80px' }}
+            container
+            spacing={2}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           >
-            <Typography variant="h4">
-              Improve fleet management with truck dispatch service by partnering
-              with a freight dispatcher, navigating the numerous companies
-              available.
-            </Typography>
-            <Button>
-              <Button type="tel" >
-                +1(210) 963 5208{" "}
-              </Button>
-            </Button>
+            <Grid
+              item
+              md="6"
+              sm="12"
+              sx={{ marginTop: "60px", padding: "0 10px" }}
+            >
+              <Typography
+                variant="h1"
+                sx={{ margin: "40px 0 auto", fontWeight: 400, color: "#fff" }}
+              >
+                FREIGHT DISPATCH SERVICE
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              md="6"
+              sm="12"
+              sx={{ fontWeight: 400, color: "#fff", marginTop: "80px" }}
+            >
+              <Typography variant="h4">
+                Improve fleet management with truck dispatch service by
+                partnering with a freight dispatcher, navigating the numerous
+                companies available.
+              </Typography>
+              <Button  sx={{ background: "#003366", color:'#fff', padding:'4px 15px' }}>+1(210) 963 5208</Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
+      <About />
+      <Services style={{ background: "#000" }} />
+      <Contact />
     </Box>
   );
 };
